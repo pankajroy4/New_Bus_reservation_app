c54d12aade70a78ff49dd2285b6240c43d6012da
pankajroy4 <pankajroy842002@gmail.com>
2024-03-06 23:25:24 +0530

    Login process chnaged and timer for otp added

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,37 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
-  def new
-    super
-  end
-
-  def otp_verification
-    sign_out(current_user) if current_user
-    @email = params[:email]
-    @user = User.find_by(email: @email)
-    @remember_me = params[:remember_me]
-    if (@user&.valid_password?(params[:password]) && @user&.confirmed?)
-      @user.generate_and_send_otp
-      flash.now[:notice] = "A new OTP has been sent to your email."
-      respond_to do |format|
-        # format.json { render json: { otp: User.find(@user.id).otp } }
-        format.html { render :otp_verification }
-        format.turbo_stream {
-          render turbo_stream: turbo_stream.update("otp", partial: "users/sessions/otp_verification", locals: { email: @email, remember_me: @remember_me })
-        }
-      end
-      
-    else
-      if @user
-        if @user.confirmed?
-          flash.now[:alert] = "Invalid  password!"
-        else
-          flash.now[:alert] = "You have to confirm your email first!"
-        end
-      else
-        flash.now[:alert] = "Invalid email or password!"
-      end
-      render :new, status: :unprocessable_entity
-    end
-  end
 
   def resend_otp
     @email = params[:email]
@@ -40,10 +7,7 @@ class Users::SessionsController < Devise::SessionsController
     if user
       user.generate_and_send_otp
       flash.now[:notice] = "OTP resended, check your mail!"
-      respond_to do |format|
-        format.html { render :otp_verification, status: :ok }
-        format.turbo_stream { flash.now[:notice] = "OTP resended, check your mail please!." }
-      end
+      render turbo_stream: turbo_stream.update("otp", partial: "users/sessions/otp_verification", locals: { email: @email, remember_me: @remember_me }) #will no longer needed after handling onclick
     else
       redirect_to root_path, alert: "Invalid email address."
     end
@@ -53,21 +17,40 @@ class Users::SessionsController < Devise::SessionsController
     @email = params[:email]
     user = User.find_by(email: @email)
     @remember_me = params[:remember_me]
-    if user && user.valid_otp?(params[:otp])
-      user.update!(remember_me: @remember_me)
-      sign_in(:user, user)
-      if user.admin?
-        redirect_to admin_show_path(user.id), notice: "Admin logged in successfully!"
+
+    if (params[:otp_step])
+      if user && user.valid_otp?(params[:otp])
+        user.update!(remember_me: @remember_me)
+        sign_in(:user, user)
+        if user.admin?
+          redirect_to admin_show_path(user.id), notice: "Admin logged in successfully!"
+        else
+          redirect_to after_sign_in_path_for(user), notice: "Logged in successfully!"
+        end
       else
-        redirect_to after_sign_in_path_for(user), notice: "Logged in successfully!"
+        flash.now[:alert] = "Invalid email or OTP."
+        render turbo_stream: turbo_stream.update("otp", partial: "users/sessions/otp_verification", locals: { email: @email, remember_me: @remember_me })
       end
     else
-      flash.now[:alert] = "Invalid email or OTP."
-      respond_to do |format|
-        format.html { render :otp_verification, status: :unprocessable_entity }
-        format.turbo_stream {
-          render turbo_stream: turbo_stream.update("otp", partial: "users/sessions/otp_verification", locals: { email: @email, remember_me: @remember_me })
-        }
+      sign_out(current_user) if current_user
+      @email = params[:email]
+      @user = User.find_by(email: @email)
+      @remember_me = params[:remember_me]
+      if (@user&.valid_password?(params[:password]) && @user&.confirmed?)
+        @user.generate_and_send_otp
+        flash.now[:notice] = "A new OTP has been sent to your email."
+        render turbo_stream: turbo_stream.update("otp", partial: "users/sessions/otp_verification", locals: { email: @email, remember_me: @remember_me })
+      else
+        if @user
+          if @user.confirmed?
+            flash.now[:alert] = "Invalid  password!"
+          else
+            flash.now[:alert] = "You have to confirm your email first!"
+          end
+        else
+          flash.now[:alert] = "Invalid email or password!"
+        end
+        render :new, status: :unprocessable_entity
       end
     end
   end
@@ -77,6 +60,6 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:otp])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:otp, :otp_step])
   end
 end
